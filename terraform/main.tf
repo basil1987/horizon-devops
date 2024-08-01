72bfd15a5292956f433338299dd500617aeefdf7
basil1987 <basil1987@gmail.com>
2024-08-01 21:02:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,11 +43,11 @@ resource "aws_security_group" "webservers" {
 }
 
 resource "aws_instance" "web001" {
-  count = "5"
-  ami           = "ami-0851b76e8b1bce90b"
+  count = "2"
+  ami           = "ami-0ad21ae1d0696ad58"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.webservers.id ]
-  key_name = "priceline-ansible-demo"
+  key_name = "ansible"
   tags = {
     Name = "Web-${count.index + 1}"
     type = "webserver"
